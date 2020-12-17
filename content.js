5b0d02d9baf5bcdac4699e95dc3d1c384fb5cf33
Henry <36207470+henryinqz@users.noreply.github.com>
2020-12-17 17:35:38 -0500

    first commit

---
+++ content.js
@@ -0,0 +1,11 @@
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        if(request.message === "clicked_browser_action") {
+            var iframeSrc = document.getElementById("doc_preview").getElementsByTagName("iframe")[0].src;
+            
+            console.log(iframeSrc);
+
+            chrome.runtime.sendMessage({"message": "open_new_tab", "url": iframeSrc});
+        }
+    }
+);
