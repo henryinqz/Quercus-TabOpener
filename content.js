4230f1c3eed1abdbcef39de16840a4336ad2f902
Henry <36207470+henryinqz@users.noreply.github.com>
2021-01-04 21:46:29 -0500

    Set manifest version to 1.0

--- content.js
+++ content.js
@@ -1,5 +1,4 @@
 if (document.getElementById("doc_preview") != null) { // Check if doc_preview exists
-    // console.log("doc_preview exists")
     chrome.runtime.sendMessage({"message": "badge_activity", "status": true}); // Enable green badge on browser action/extension icon (indicates a module can be opened)
 }
 
@@ -7,8 +6,7 @@ chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
         if(request.message === "clicked_browser_action" && document.getElementById("doc_preview") != null) {
             var docViewerLink = document.getElementById("doc_preview").getElementsByTagName("iframe")[0].src;
-            // console.log(docViewerLink);
-
+            
             chrome.runtime.sendMessage({"message": "open_new_tab", "url": docViewerLink});
         }
     }
